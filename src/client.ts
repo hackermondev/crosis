b2db3bdcd22fa6ef7e2f23fbeea6a8b81387c7d8
lhchavez <lhchavez@lhchavez.com>
2020-12-01 12:51:51 -0800

    Allow specifying a different URL to connect to in the EIO compat
    This change appends the original URL that we were supposed to connect to
    if we are going to go through the EIO compat route. This allows for
    multi-cluster support!
    
    Depends on: https://github.com/replit/goval-proxy-v2/pull/2

--- src/client.ts
+++ src/client.ts
@@ -129,12 +129,17 @@ export class Client extends EventEmitter {
 
   private didConnect: boolean;
 
-  static getConnectionStr(connectionMetadata: GovalMetadata, pollingHost?: string) {
+  static getConnectionStr(connectionMetadata: GovalMetadata, pollingHost?: string): string {
     const gurl = urllib.parse(connectionMetadata.gurl);
     if (pollingHost) {
+      gurl.hostname = pollingHost;
       gurl.host = pollingHost;
+      gurl.pathname = `/wsv2/${connectionMetadata.token}/${encodeURIComponent(
+        connectionMetadata.gurl,
+      )}`;
+    } else {
+      gurl.pathname = `/wsv2/${connectionMetadata.token}`;
     }
-    gurl.pathname = `/wsv2/${connectionMetadata.token}`;
     return urllib.format(gurl);
   }
 
